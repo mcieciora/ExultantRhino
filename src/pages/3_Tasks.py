3dfb261429288330f3932b05e0a63342dd8880b8
mcieciora <mcieciora@nowhere.com>
2024-06-27 20:40:01 +0200

    27062024-2039

--- src/pages/3_Tasks.py
+++ src/pages/3_Tasks.py
@@ -1,6 +1,5 @@
-from pandas import DataFrame
 from streamlit import button, column_config, dataframe, header, selectbox, sidebar, success, text_area, text_input, \
-    query_params
+    warning, query_params
 from src.postgres_items_models import Project, Release
 from src.postgres_tasks_models import Task, TaskStatus
 from src.postgres_sql_alchemy import edit_database_object, get_all_objects_by_type, get_objects_by_filters
@@ -26,7 +25,7 @@ current_project = sidebar.selectbox(
 )
 
 
-def update_task(task_id):
+def update_task(form_dict, task_id):
     """
     Update task with form_dict data by given id.
 
@@ -35,42 +34,65 @@ def update_task(task_id):
     edit_database_object(Task, task_id, form_dict)
 
 
+def page(edit_page):
+    """
+    Main page function.
+
+    :return: None
+    """
+    if edit_page:
+        statuses = [status.name for status in TaskStatus]
+        status_index = statuses.index(task["status"])
+        form_dict = {
+            "title": text_input("Title", key="task_title", value=task["title"]),
+            "description": text_area("Description", key="task_description", value=task["description"]),
+            "status": selectbox("Status", key="task_status", options=statuses, index=status_index),
+        }
+
+        if button(label="Update", key="task_update_button", on_click=update_task, args=(form_dict, task["id"],)):
+            success(f"Updated {task['title']}")
+    else:
+        header("Tasks")
+        try:
+            target_release = get_objects_by_filters(Release, {"status": "Active"})[0]
+            all_objects = get_objects_by_filters(Task, {"project_shortname": current_project.split(':')[0],
+                                                        "target_release": target_release["shortname"]})
+
+            for item in all_objects:
+                item["url"] = f"http://localhost:8501/Tasks?item={item['shortname']}"
+
+            dataframe(all_objects,
+                      column_config={
+                          "shortname": "Shortname",
+                          "title": "Title",
+                          "description": "Description",
+                          "status": "Status",
+                          "parent": "Parent",
+                          "url": column_config.LinkColumn("View URL", display_text="View")
+                      },
+                      column_order=("shortname", "title", "description", "status", "url"),
+                      hide_index=True
+                      )
+        except IndexError:
+            header("No active release.")
+
+
+def not_found():
+    """
+    Not found page.
+
+    :return: None
+    """
+    warning("Task not found")
+
+
 parameters = query_params
 if parameters:
-    task = get_objects_by_filters(Task, {"shortname": parameters["item"]})[0]
-    statuses = [status.name for status in TaskStatus]
-    status_index = statuses.index(task["status"])
-    form_dict = {
-        "title": text_input("Title", key="task_title", value=task["title"]),
-        "description": text_area("Description", key="task_description", value=task["description"]),
-        "status": selectbox("Status", key="task_status", options=statuses, index=status_index),
-    }
-
-    if button(label="Update", key="task_update_button", on_click=update_task, args=(task["id"],)):
-        success(f"Updated {task['title']}")
-else:
-    header("Tasks")
+    header("Edit task")
     try:
-        target_release = get_objects_by_filters(Release, {"status": "Active"})[0]
-        all_objects = get_objects_by_filters(Task, {"project_shortname": current_project.split(':')[0],
-                                                    "target_release": target_release["shortname"]})
-
-        for item in all_objects:
-            item["url"] = f"http://localhost:8501/Tasks?item={item['shortname']}"
-
-        df = DataFrame(all_objects)
-
-        dataframe(all_objects,
-                  column_config={
-                      "shortname": "Shortname",
-                      "title": "Title",
-                      "description": "Description",
-                      "status": "Status",
-                      "parent": "Parent",
-                      "url": column_config.LinkColumn("View URL", display_text="View")
-                  },
-                  column_order=("shortname", "title", "description", "status", "url"),
-                  hide_index=True
-                  )
+        task = get_objects_by_filters(Task, {"shortname": parameters["item"]})[0]
+        page(edit_page=True)
     except IndexError:
-        header("No active release.")
+        not_found()
+else:
+    page(edit_page=False)
