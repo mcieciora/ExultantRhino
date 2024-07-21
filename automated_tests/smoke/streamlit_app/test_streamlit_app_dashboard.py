47417fdfdae0c0e0b835edb67df54c4e221eb760
mcieciora <mcieciora@nowhere.com>
2024-07-21 11:45:27 +0200

    21072024-1145

--- automated_tests/smoke/streamlit_app/test_streamlit_app_dashboard.py
+++ automated_tests/smoke/streamlit_app/test_streamlit_app_dashboard.py
@@ -8,13 +8,13 @@ def test__smoke__streamlit_app__dashboard_nav(two_fully_set_up_projects, seleniu
     expected_values = [2, 3, 5, 2]
     test_project_shortname = "new_project"
     for index, value in enumerate(selenium_util.driver.find_elements(By.XPATH, "//*[@data-testid='stMetricValue']")):
-        actual_value = eval(value.text)
+        actual_value = str(value.text)
         expected_value = expected_values[index]
         assert actual_value == expected_value, f"Expected: {expected_value}, actual: {actual_value}"
     selenium_util.choose_from_select_box("Selected DEFAULT. current_project_select_box", test_project_shortname)
     expected_values = [1, 2, 3, 2]
     for index, value in enumerate(selenium_util.driver.find_elements(By.XPATH, "//*[@data-testid='stMetricValue']")):
-        actual_value = eval(value.text)
+        actual_value = str(value.text)
         expected_value = expected_values[index]
         assert actual_value == expected_value, f"Expected: {expected_value}, actual: {actual_value}"
 
